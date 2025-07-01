25b8b9fe26196b14a6bc5e6156fe9d2b5fbbf4ee
Albert Rodríguez <albert.rodriguez@in2.es>
2025-07-01 11:21:01 +0200

    fix(flyway): fix sql

--- src/main/resources/db/migration/V4__Create_status_list_index_table.sql
+++ src/main/resources/db/migration/V4__Create_status_list_index_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS issuer.status_list_index (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    nonce UUID NOT NULL uuid_generate_v4(),
+    nonce UUID NOT NULL,
     list_id INTEGER NOT NULL
     );
