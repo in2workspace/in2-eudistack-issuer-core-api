884c54ed0dc62a0c6677c4a98e2b4350b732294e
Albert Rodriguez <albert.rodriguez@in2.es>
2025-08-07 10:48:56 +0200

    fix machine signature

---
+++ src/main/java/es/in2/issuer/backend/shared/domain/model/dto/LEARCredentialMachineJwtPayload.java
@@ -0,0 +1,30 @@
+package es.in2.issuer.backend.shared.domain.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import es.in2.issuer.backend.shared.domain.model.dto.credential.lear.machine.LEARCredentialMachine;
+import lombok.Builder;
+
+@Builder
+public record LEARCredentialMachineJwtPayload(
+        @JsonProperty("sub")
+        String subject,
+
+        @JsonProperty("nbf")
+        Long notValidBefore,
+
+        @JsonProperty("iss")
+        String issuer,
+
+        @JsonProperty("exp")
+        Long expirationTime,
+
+        @JsonProperty("iat")
+        Long issuedAt,
+
+        @JsonProperty("vc")
+        LEARCredentialMachine learCredentialMachine,
+
+        @JsonProperty("jti")
+        String JwtId
+) {
+}
