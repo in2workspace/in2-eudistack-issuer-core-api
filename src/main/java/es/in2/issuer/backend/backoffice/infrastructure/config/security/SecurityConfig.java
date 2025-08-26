b0c747b5098d7a2c7e2ef2f3c1398db363d81708
Mike <miguel.mir@in2.es>
2025-08-26 02:59:39 +0200

    (issuer) [2.x] refactor securityConfig - added logs

--- src/main/java/es/in2/issuer/backend/backoffice/infrastructure/config/security/SecurityConfig.java
+++ src/main/java/es/in2/issuer/backend/backoffice/infrastructure/config/security/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig {
     private final CustomAuthenticationManager customAuthenticationManager;
     private final InternalCORSConfig internalCORSConfig;
     private final PublicCORSConfig publicCORSConfig;
+    private final ReactiveJwtDecoder internalJwtDecoder;
 
     @Bean
     @Primary
@@ -118,6 +119,7 @@ public class SecurityConfig {
                         .anyExchange().denyAll()
                 )
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtDecoder(internalJwtDecoder)))
                 .addFilterAt(customAuthenticationWebFilter(), SecurityWebFiltersOrder.AUTHENTICATION);
         log.debug("backofficeFilterChain - build");
         return http.build();
