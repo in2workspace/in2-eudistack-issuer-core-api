1ce1739fac02e5af6467eb14da7ca9bdb60f1bde
Mike <miguel.mir@in2.es>
2025-08-27 10:39:31 +0200

    (issuer) [2.x] refactor securityConfig - update paths - NotificationController

--- src/main/java/es/in2/issuer/backend/backoffice/infrastructure/controller/NotificationController.java
+++ src/main/java/es/in2/issuer/backend/backoffice/infrastructure/controller/NotificationController.java
@@ -12,7 +12,7 @@ import java.util.UUID;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/v1/notifications")
+@RequestMapping("/backoffice/v1/notifications")
 @RequiredArgsConstructor
 public class NotificationController {
 
