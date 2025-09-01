9aaff345010acd7e19794e2058480393b05d6808
Roger Miret <roger.miret@in2.es>
2025-09-01 10:15:54 +0200

    test log

--- src/main/java/es/in2/issuer/backend/backoffice/infrastructure/config/security/JwtDecoderConfig.java
+++ src/main/java/es/in2/issuer/backend/backoffice/infrastructure/config/security/JwtDecoderConfig.java
@@ -1,6 +1,7 @@
 package es.in2.issuer.backend.backoffice.infrastructure.config.security;
 
 import es.in2.issuer.backend.shared.infrastructure.config.AuthServerConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.jose.jws.SignatureAlgorithm;
@@ -8,6 +9,7 @@ import org.springframework.security.oauth2.jwt.JwtValidators;
 import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 
+@Slf4j
 @Configuration
 public class JwtDecoderConfig {
 
@@ -19,6 +21,8 @@ public class JwtDecoderConfig {
 
     @Bean
     public ReactiveJwtDecoder internalJwtDecoder() {
+//        todo remove
+        log.info("ReactiveJwtDecoder");
         NimbusReactiveJwtDecoder jwtDecoder = NimbusReactiveJwtDecoder
                 .withJwkSetUri(authServerConfig.getJwtDecoder())
                 .jwsAlgorithm(SignatureAlgorithm.RS256)
