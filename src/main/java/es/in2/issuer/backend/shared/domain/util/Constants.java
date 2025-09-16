0849064e7d22ce3e41f624e56a5414ec3a778f7f
Roger Miret <roger.miret@in2.es>
2025-09-16 12:26:29 +0200

    update CREDENTIAL_CONTEXT_LEAR_CREDENTIAL_MACHINE

--- src/main/java/es/in2/issuer/backend/shared/domain/util/Constants.java
+++ src/main/java/es/in2/issuer/backend/shared/domain/util/Constants.java
@@ -19,7 +19,7 @@ public final class Constants {
     public static final List<String> LEAR_CREDENTIAL_EMPLOYEE_CONTEXT = List.of(CREDENTIALS_CONTEXT_V2, "https://www.dome-marketplace.eu/2025/credentials/learcredentialemployee/v2");
     public static final List<String> LABEL_CREDENTIAL_CONTEXT = List.of(CREDENTIALS_CONTEXT_V2, "https://w3id.org/gaia-x/development#");
     public static final List<String> CREDENTIAL_CONTEXT_LEAR_CREDENTIAL_EMPLOYEE = List.of(CREDENTIALS_CONTEXT_V2, "https://www.dome-marketplace.eu/2025/credentials/learcredentialemployee/v2");
-    public static final List<String> CREDENTIAL_CONTEXT_LEAR_CREDENTIAL_MACHINE = List.of(CREDENTIALS_CONTEXT_V2, "https://www.dome-marketplace.eu/2025/credentials/learcredentialmachine/v2");
+    public static final List<String> CREDENTIAL_CONTEXT_LEAR_CREDENTIAL_MACHINE = List.of(CREDENTIALS_CONTEXT_V2, "https://credentials.eudistack.eu/.well-known/credentials/lear_credential_machine/w3c/v2");
     // EXPIRATION TIMES
     public static final Integer CREDENTIAL_OFFER_CACHE_EXPIRATION_TIME = 10;
     public static final Integer VERIFIABLE_CREDENTIAL_JWT_CACHE_EXPIRATION_TIME = 10;
