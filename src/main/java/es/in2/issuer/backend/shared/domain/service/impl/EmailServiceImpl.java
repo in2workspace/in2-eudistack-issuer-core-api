764256cec5e7dfec276c9011c30482e37eb46cb8
Roger Miret <roger.miret@in2.es>
2025-10-17 13:07:16 +0200

    EmailServiceImpl: ensure UTF-8 Subject

--- src/main/java/es/in2/issuer/backend/shared/domain/service/impl/EmailServiceImpl.java
+++ src/main/java/es/in2/issuer/backend/shared/domain/service/impl/EmailServiceImpl.java
@@ -8,6 +8,7 @@ import es.in2.issuer.backend.shared.domain.service.TranslationService;
 import es.in2.issuer.backend.shared.infrastructure.config.AppConfig;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
+import jakarta.mail.internet.MimeUtility;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.mail.MailProperties;
@@ -25,6 +26,7 @@ import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 import static es.in2.issuer.backend.backoffice.domain.util.Constants.*;
 
@@ -44,7 +46,9 @@ public class EmailServiceImpl implements EmailService {
             MimeMessage mimeMessage = javaMailSender.createMimeMessage();
             MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, UTF_8);
             helper.setFrom(mailProperties.getUsername());
-            helper.setTo(to);
+            String translated = translationService.translate(subject);
+            String encodedSubject = MimeUtility.encodeText(translated, StandardCharsets.UTF_8.name(), "B");
+            helper.setSubject(encodedSubject);
             helper.setSubject(translationService.translate(subject));
 
             Context context = new Context();
