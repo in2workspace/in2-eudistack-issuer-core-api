7ac477f51653ad6e9461a4fc4ee245393b1ef8b3
Roger Miret <roger.miret@in2.es>
2026-01-28 18:23:51 +0100

    remove credentialStatus classes in backoffice

--- src/main/java/es/in2/issuer/backend/backoffice/domain/model/dtos/CredentialStatusResponse.java
+++
@@ -1,7 +0,0 @@
-package es.in2.issuer.backend.backoffice.domain.model.dtos;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public record CredentialStatusResponse(
-        @JsonProperty("nonce") String credentialNonce) {
-}
