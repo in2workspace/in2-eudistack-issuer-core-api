ff12cf1aeb90bfca45631de38b2c3959a37c6019
Roger Miret <roger.miret@in2.es>
2026-02-02 17:19:47 +0100

    fix(Preconditions): avoid infinite loop

--- src/main/java/es/in2/issuer/backend/statuslist/domain/util/Preconditions.java
+++ src/main/java/es/in2/issuer/backend/statuslist/domain/util/Preconditions.java
@@ -1,5 +1,7 @@
 package es.in2.issuer.backend.statuslist.domain.util;
 
+import static java.util.Objects.requireNonNull;
+
 public final class Preconditions {
 
     private static final String CANNOT_BE_NULL = " cannot be null";
@@ -9,6 +11,6 @@ public final class Preconditions {
     }
 
     public static <T> T requireNonNullParam(T value, String name) {
-        return requireNonNullParam(value, name + CANNOT_BE_NULL);
+        return requireNonNull(value, name + CANNOT_BE_NULL);
     }
 }
